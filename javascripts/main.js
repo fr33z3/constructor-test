62c9dd8daa6b5bd4c6be55864fb06e00959d0571
Sergey Gribovski <sgribovski@gmail.com>
2015-01-06 14:52:36 +0100

    drag functions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,23 @@
-constructorListener = function(event) {
-  alert(event.origin + ' ' + event.data);
+// constructorListener = function(event) {
+//   alert(event.origin + ' ' + event.data);
+// }
+
+// if (window.addEventListener) {
+//   addEventListener("message", constructorListener, false)
+// } else {
+//   attachEvent("onmessage", constructorListener)
+// }
+
+dragOverHandler = function(event) {
+  console.log(event);
 }
 
-if (window.addEventListener) {
-  addEventListener("message", constructorListener, false)
-} else {
-  attachEvent("onmessage", constructorListener)
+dropHandler = function(event) {
+  event.preventDefault();
 }
+
+var body = document.getElementsByTagName("body")[0]
+
+body.addEventListener('ondragover', dragOverHandler);
+body.addEventListener('ondrop', dropHandler);
+
